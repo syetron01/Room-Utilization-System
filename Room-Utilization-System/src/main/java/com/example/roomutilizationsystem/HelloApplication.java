36bb3d1c85e613f5e68aab26c272261d30aea507
Ukbeep <hxdariel@gmail.com>
2025-04-11 00:55:17 +0800

    2st commit - with css file(no contents)

--- Room-Utilization-System/src/main/java/com/example/roomutilizationsystem/HelloApplication.java
+++ Room-Utilization-System/src/main/java/com/example/roomutilizationsystem/HelloApplication.java
@@ -10,10 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("staffViewAvailableRoom.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("logIn.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
